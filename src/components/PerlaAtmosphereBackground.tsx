8fae2c8e69df156cd34a00813066e7d4b9cad26d
ala-1998 <benrejabala@gmail.com>
2026-09-14 00:19:57 +0200

    pearl

--- src/components/PerlaAtmosphereBackground.tsx
+++ src/components/PerlaAtmosphereBackground.tsx
@@ -24,43 +24,50 @@ export const PerlaAtmosphereBackground: React.FC = () => {
         <WhatsAppBodySilhouette className="w-full h-full object-contain filter drop-shadow-sm" />
       </div>
 
-      {/* 4. 3D Radiant Pearlescent Spheres & Crystal Water Bubbles ("fo9a3at mtaa mee") */}
+            {/* 4. 3D Radiant Powder Rose Pearlescent Spheres & Crystal Water Bubbles */}
       
-      {/* Top-Left: Large Radiant Pearl Sphere */}
-      <div className="absolute -top-10 -left-10 sm:-top-8 sm:-left-8 w-36 h-36 sm:w-48 sm:h-48 pearl-sphere animate-float-1 z-1 opacity-90">
-        <div className="absolute inset-0 rounded-full bg-gradient-to-tr from-[#D8C4BA]/20 via-transparent to-white/60 pointer-events-none" />
+      {/* Top-Left: Large Radiant Powder Rose Pearl Sphere */}
+      <div className="absolute -top-10 -left-10 sm:-top-8 sm:-left-8 w-36 h-36 sm:w-48 sm:h-48 pearl-sphere animate-float-1 z-1 opacity-95">
+        <div className="absolute inset-0 rounded-full bg-gradient-to-tr from-[#C9A6A5]/25 via-transparent to-white/60 pointer-events-none" />
       </div>
 
+      {/* Mid-Field Upper Pearl (from user screenshot): Powder Rose with Soft Glow */}
+      <div className="absolute top-[32%] right-[34%] sm:right-[38%] w-12 h-12 sm:w-16 sm:h-16 pearl-sphere animate-float-2 z-1 opacity-95" />
+
+      {/* Mid-Field Lower Pearl (from user screenshot, beside heading): Powder Rose with Soft Glow */}
+      <div className="absolute top-[48%] right-[16%] sm:right-[20%] w-14 h-14 sm:w-18 sm:h-18 pearl-sphere animate-float-3 z-1 opacity-95" />
+
       {/* Top-Center/Right: Crystal Translucent Water Bubble */}
-      <div className="absolute top-20 right-[28%] sm:right-[32%] w-14 h-14 sm:w-20 sm:h-20 water-bubble animate-float-2 z-1">
+      <div className="absolute top-20 right-[24%] sm:right-[28%] w-12 h-12 sm:w-16 sm:h-16 water-bubble animate-float-2 z-1">
         <div className="absolute top-2 left-2 w-3 h-2 rounded-full bg-white/80 blur-[0.5px] transform -rotate-45" />
       </div>
 
       {/* Near Upper-Right Silhouette: Delicate Micro Water Bubble */}
-      <div className="absolute top-36 right-[12%] w-8 h-8 sm:w-11 sm:h-11 water-bubble animate-float-3 z-1 opacity-85" />
+      <div className="absolute top-36 right-[10%] w-8 h-8 sm:w-10 sm:h-10 water-bubble animate-float-3 z-1 opacity-85" />
 
-      {/* Mid-Left: Floating Pearlescent Sphere */}
-      <div className="absolute top-[38%] -left-6 sm:left-6 w-20 h-20 sm:w-28 sm:h-28 pearl-sphere animate-float-2 z-1 opacity-85" />
+      {/* Mid-Left: Floating Powder Rose Pearlescent Sphere */}
+      <div className="absolute top-[38%] -left-6 sm:left-6 w-20 h-20 sm:w-28 sm:h-28 pearl-sphere animate-float-2 z-1 opacity-90" />
 
       {/* Mid-Page: Translucent Water Bubble with shimmer */}
       <div className="absolute top-[42%] left-[22%] sm:left-[28%] w-12 h-12 sm:w-16 sm:h-16 water-bubble animate-float-1 z-1" />
 
       {/* Mid-Right: Floating Water Bubble */}
-      <div className="absolute top-[55%] right-[8%] sm:right-[15%] w-16 h-16 sm:w-24 sm:h-24 water-bubble animate-float-3 z-1">
+      <div className="absolute top-[60%] right-[8%] sm:right-[12%] w-14 h-14 sm:w-20 sm:h-20 water-bubble animate-float-3 z-1">
         <div className="absolute top-3 left-3 w-4 h-2.5 rounded-full bg-white/90 blur-[0.5px] transform -rotate-45" />
       </div>
 
       {/* Center-Lower: Small Floating Water Bubble */}
       <div className="absolute top-[68%] left-[45%] w-9 h-9 sm:w-12 sm:h-12 water-bubble animate-float-2 z-1 opacity-80" />
 
-      {/* Bottom-Left: Large Shimmering Pearl Sphere */}
-      <div className="absolute -bottom-12 -left-8 sm:bottom-12 sm:left-12 w-32 h-32 sm:w-44 sm:h-44 pearl-sphere animate-float-3 z-1 opacity-90" />
+      {/* Bottom-Left: Large Shimmering Powder Rose Pearl Sphere */}
+      <div className="absolute -bottom-12 -left-8 sm:bottom-12 sm:left-12 w-32 h-32 sm:w-44 sm:h-44 pearl-sphere animate-float-3 z-1 opacity-95" />
 
       {/* Bottom-Left Adjacent: Crystal Water Bubble */}
       <div className="absolute bottom-32 left-[18%] sm:left-[22%] w-10 h-10 sm:w-14 sm:h-14 water-bubble animate-float-1 z-1" />
 
-      {/* Bottom-Right: Radiant Pearl Sphere */}
-      <div className="absolute -bottom-16 -right-10 sm:-bottom-10 sm:-right-8 w-36 h-36 sm:w-52 sm:h-52 pearl-sphere animate-float-1 z-1 opacity-90" />
+      {/* Bottom-Right: Radiant Powder Rose Pearl Sphere */}
+      <div className="absolute -bottom-16 -right-10 sm:-bottom-10 sm:-right-8 w-36 h-36 sm:w-52 sm:h-52 pearl-sphere animate-float-1 z-1 opacity-95" />
+
 
       {/* Bottom-Right Adjacent: Translucent Water Bubble */}
       <div className="absolute bottom-40 right-[24%] w-12 h-12 sm:w-16 sm:h-16 water-bubble animate-float-2 z-1" />
